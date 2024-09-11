edc16728fac8f6728b9949cfb15b4c22e271178e
YunByungil <dbsquddlfz@naver.com>
2024-09-11 22:52:35 +0900

    first commit

---
+++ src/main/java/com/hongik/HongikApplication.java
@@ -0,0 +1,13 @@
+package com.hongik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongikApplication.class, args);
+	}
+
+}
