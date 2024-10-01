6d59b1e14eb49d07cd8bcea97d45a914158b2159
YunByungil <dbsquddlfz@naver.com>
2024-10-01 17:18:10 +0900

    feat: 서버 기준 시간으로 연간, 월간, 투데이, 학기 공부 시간 조회 기능 구현

--- src/main/java/com/hongik/domain/study/StudySessionRepository.java
+++ src/main/java/com/hongik/domain/study/StudySessionRepository.java
@@ -9,7 +9,10 @@ import java.util.List;
 
 public interface StudySessionRepository extends JpaRepository<StudySession, Long> {
 
-    @Query(value = "SELECT SUM(TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time)) " +
+    /**
+     * 2024년 10월 1일에 대한 공부 시간을 조회한다.
+     */
+    @Query(value = "SELECT IFNULL(SUM(TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time)), 0) " +
             "FROM study_session s " +
             "WHERE s.user_id = :userId " +
             "AND YEAR(s.start_time) = :year " +
@@ -19,15 +22,54 @@ public interface StudySessionRepository extends JpaRepository<StudySession, Long
                                 @Param("year") int year,
                                 @Param("month") int month,
                                 @Param("day") int day);
+//    @Query(value = "SELECT SUM(TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time)) " +
+//            "FROM study_session s " +
+//            "WHERE s.user_id = :userId " +
+//            "AND YEAR(s.start_time) = :year " +
+//            "AND MONTH(s.start_time) = :month " +
+//            "AND DAY(s.start_time) = :day", nativeQuery = true)
+//    Long getStudyDurationForDay(@Param("userId") Long userId,
+//                                @Param("year") int year,
+//                                @Param("month") int month,
+//                                @Param("day") int day);
 
-    @Query(value = "SELECT SUM(TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time)) " +
+    /**
+     * 2024년 10월에 대한 공부 시간을 조회한다.
+     */
+    @Query(value = "SELECT IFNULL(SUM(TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time)), 0)" +
             "FROM study_session s " +
             "WHERE s.user_id = :userId " +
             "AND YEAR(s.start_time) = :year " +
             "AND MONTH(s.start_time) = :month", nativeQuery = true)
     Long getStudyDurationForMonth(@Param("userId") Long userId,
-                                @Param("year") int year,
-                                @Param("month") int month);
+                                  @Param("year") int year,
+                                  @Param("month") int month);
+
+    /**
+     * 2024년에 대한 공부 시간을 조회한다.
+     */
+    @Query(value = "SELECT IFNULL(SUM(TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time)), 0)" +
+            "FROM study_session s " +
+            "WHERE s.user_id = :userId " +
+            "AND YEAR(s.start_time) = :year", nativeQuery = true)
+    Long getStudyDurationForYear(@Param("userId") Long userId,
+                                 @Param("year") int year);
+
+    /**
+     * 2024년 학기에 대한 공부 시간을 조회한다.
+     */
+    @Query(value = "SELECT " +
+            "CASE " +
+            "WHEN :semester = 'winter' THEN SUM(CASE WHEN YEAR(s.start_time) = :year AND MONTH(s.start_time) IN (1, 2) THEN TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time) ELSE 0 END) " +
+            "WHEN :semester = 'first' THEN SUM(CASE WHEN YEAR(s.start_time) = :year AND MONTH(s.start_time) IN (3, 4, 5, 6) THEN TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time) ELSE 0 END) " +
+            "WHEN :semester = 'summer' THEN SUM(CASE WHEN YEAR(s.start_time) = :year AND MONTH(s.start_time) IN (7, 8) THEN TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time) ELSE 0 END)" +
+            "WHEN :semester = 'second' THEN SUM(CASE WHEN YEAR(s.start_time) = :year AND MONTH(s.start_time) IN (9, 10, 11, 12) THEN TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time) ELSE 0 END) " +
+            "ELSE 0 END AS semesterStudyTime " +
+            "FROM study_session s " +
+            "WHERE s.user_id = :userId", nativeQuery = true)
+    Long getStudyDurationForSemester(@Param("userId") Long userId,
+                                     @Param("year") int year,
+                                     @Param("semester") String semester);
 
     @Query(value = "SELECT DATE(s.start_time) AS date, COUNT(*) AS studyCount " +
             "FROM study_session s " +
@@ -36,8 +78,8 @@ public interface StudySessionRepository extends JpaRepository<StudySession, Long
             "AND MONTH(s.start_time) = :month " +
             "GROUP BY DATE(s.start_time)", nativeQuery = true)
     List<Object[]> getStudyCountByMonth(@Param("userId") Long userId,
-                                                  @Param("year") int year,
-                                                  @Param("month") int month);
+                                        @Param("year") int year,
+                                        @Param("month") int month);
 
     @Query(value = "SELECT DATE(s.start_time) AS date, COUNT(*) AS studyCount " +
             "FROM study_session s " +
@@ -51,4 +93,25 @@ public interface StudySessionRepository extends JpaRepository<StudySession, Long
             "and DATE(s.start_time) in :dates " +
             "GROUP BY DATE(s.start_time)", nativeQuery = true)
     List<Object[]> getStudyCountByWeek(@Param("userId") Long userId, List<LocalDate> dates);
+
+    /**
+     * 2024년 10월 01일 기준
+     * 2024년 공부 시간
+     * 2024년 10월 공부시간
+     * 2024년 10월 1일 공부시간을 측정한다.
+     */
+    @Query(value = "SELECT " +
+            "SUM(CASE WHEN YEAR(s.start_time) = :year THEN TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time) " +
+            "ELSE 0 " +
+            "END) as yearly " +
+            "FROM study_session s " +
+            "WHERE s.user_id = :userId", nativeQuery = true)
+    Long getTotalStudyTime(@Param("userId") Long userId, @Param("year") int year);
+//    @Query(value = "SELECT " +
+//            "SUM(CASE WHEN YEAR(s.start_time) = :year THEN TIMESTAMPDIFF(MINUTE, s.start_time, s.end_time) " +
+//            "ELSE 0 " +
+//            "END) as year " +
+//            "FROM study_session s " +
+//            "WHERE s.user_id = :userId", nativeQuery = true)
+//    Long getTotalStudyTime(@Param("userId") Long userId, @Param("year") int year, @Param("month") int month, @Param("date") LocalDate date);
 }
