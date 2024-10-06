d2deb7236366c8df5408aa978365c68fd4f7f07e
YunByungil <dbsquddlfz@naver.com>
2024-10-06 22:35:54 +0900

    refactor: WiseSayingController - ApiResponse를 ResponseEtity로 변경

--- src/main/java/com/hongik/controller/wisesaying/WiseSayingController.java
+++ src/main/java/com/hongik/controller/wisesaying/WiseSayingController.java
@@ -7,6 +7,7 @@ import com.hongik.service.wisesaying.WiseSayingService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -22,14 +23,14 @@ public class WiseSayingController {
 
     @Operation(summary = "명언들 저장", description = "명언을 여러개 한번에 저장합니다. JSON 형식입니다.")
     @PostMapping
-    public ApiResponse<List<WiseSayingResponse>> createWiseSaying(@RequestBody List<WiseSayingCreateRequest> request) {
-        return ApiResponse.ok(wiseSayingService.createWiseSaying(request));
+    public ResponseEntity<List<WiseSayingResponse>> createWiseSaying(@RequestBody List<WiseSayingCreateRequest> request) {
+        return ResponseEntity.ok(wiseSayingService.createWiseSaying(request));
     }
 
     @Operation(summary = "명언 조회", description = "명언을 하나씩 조회합니다. 순서는 서버에서 관리합니다.")
     @GetMapping
-    public ApiResponse<WiseSayingResponse> getWiseSaying() {
+    public ResponseEntity<WiseSayingResponse> getWiseSaying() {
         LocalDate now = LocalDate.now();
-        return ApiResponse.ok(wiseSayingService.getWiseSaying(now));
+        return ResponseEntity.ok(wiseSayingService.getWiseSaying(now));
     }
 }
