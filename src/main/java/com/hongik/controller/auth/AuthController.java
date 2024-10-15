0653828b4315b5f5ff663dbeea7e5a1d176a990e
YunByungil <dbsquddlfz@naver.com>
2024-10-15 17:13:02 +0900

    feat: request 값 체크

--- src/main/java/com/hongik/controller/auth/AuthController.java
+++ src/main/java/com/hongik/controller/auth/AuthController.java
@@ -33,6 +33,8 @@ public class AuthController {
     @Operation(summary = "구글, 애플 소셜 로그인", description = "구글과 애플, id_token을 넣어주세요.")
     @PostMapping("/login")
     public ApiResponse<TokenResponse> selectGoogleLoginInfo(@RequestBody LoginRequest request){
+        System.out.println("request.getSocialPlatform() = " + request.getSocialPlatform());
+        System.out.println("request.getIdToken() = " + request.getIdToken());
         return ApiResponse.ok(authService.login(request));
     }
 //    @Operation(summary = "구글, 애플 소셜 로그인", description = "구글과 애플, id_token을 넣어주세요.")
