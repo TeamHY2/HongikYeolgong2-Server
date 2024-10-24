4f2bd6523d39722bce933d47941ff1d03508bb93
YunByungil <dbsquddlfz@naver.com>
2024-10-24 22:08:16 +0900

    fix: 최신 버전으로 적용되지 않았기 때문에 재배포

--- src/main/java/com/hongik/controller/user/UserController.java
+++ src/main/java/com/hongik/controller/user/UserController.java
@@ -39,17 +39,10 @@ public class UserController {
         return ApiResponse.ok(userService.signUp(request));
     }
 
-//    @ApiErrorCodeExamples({INVALID_INPUT_VALUE})
-//    @Operation(summary = "닉네임 중복검사", description = "닉네임 중복검사입니다.")
-//    @GetMapping("/duplicate-nickname")
-//    public ApiResponse<NicknameResponse> duplicateNickname(@Valid @RequestBody NicknameRequest request) {
-//        return ApiResponse.ok(userService.checkNicknameDuplication(request.getNickname()));
-//    }
-
     @ApiErrorCodeExamples({INVALID_INPUT_VALUE})
     @Operation(summary = "닉네임 중복검사", description = "닉네임 중복검사입니다.")
     @GetMapping("/duplicate-nickname")
-    public ApiResponse<NicknameResponse> duplicateNickname2(@NotBlank(message = "닉네임은 필수입니다.") @RequestParam String nickname) {
+    public ApiResponse<NicknameResponse> duplicateNickname(@NotBlank(message = "닉네임은 필수입니다.") @RequestParam String nickname) {
         return ApiResponse.ok(userService.checkNicknameDuplication(nickname));
     }
 
