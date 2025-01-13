51038f5cba2eef8aa0821e5772d7996d16c65648
YunByungil <dbsquddlfz@naver.com>
2025-01-13 20:09:20 +0900

    feat: TransactionalEventListener 도입

--- src/main/java/com/hongik/discord/MessageService.java
+++ src/main/java/com/hongik/discord/MessageService.java
@@ -3,43 +3,31 @@ package com.hongik.discord;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
-@Service
+@Component
 public class MessageService {
 
     @Value("${discord.url}")
     private String webhookUrl;
 
-    public boolean sendMsg(String msg){
+    @TransactionalEventListener
+    public void sendMsg(MessageEvent event) {
+        String msg = event.getMessage();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", "application/json; utf-8");
+        HttpEntity<Message> messageEntity = new HttpEntity<>(new Message(msg), httpHeaders);
 
-        try {
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.add("Content-Type", "application/json; utf-8");
-            HttpEntity<Message> messageEntity = new HttpEntity<>(new Message(msg), httpHeaders);
-
-            RestTemplate template = new RestTemplate();
-            ResponseEntity<String> response = template.exchange(
-                    webhookUrl,
-                    HttpMethod.POST,
-                    messageEntity,
-                    String.class
-            );
-
-
-            // response에 대한 처리
-            if(response.getStatusCode().value() != HttpStatus.NO_CONTENT.value()){
-                log.error("메시지 전송 이후 에러 발생");
-                return false;
-            }
-
-        } catch (Exception e) {
-            log.error("에러 발생 :: " + e);
-            return false;
-        }
-
-        return true;
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<String> response = template.exchange(
+                webhookUrl,
+                HttpMethod.POST,
+                messageEntity,
+                String.class
+        );
     }
+
 }
