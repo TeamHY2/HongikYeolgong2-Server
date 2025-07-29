611de5139bafdb177258861e53d5bcd3d9ee6f20
sunkon25 <kcindy325k@kyonggi.ac.kr>
2025-07-29 17:44:45 +0900

    refactor: 열람실 API와 학과별 랭킹 API 분리

--- src/test/java/com/hongik/controller/study/StudySessionControllerTest.java
+++ src/test/java/com/hongik/controller/study/StudySessionControllerTest.java
@@ -58,7 +58,7 @@ class StudySessionControllerTest {
                 .build();
 
         // when // then
-        mockMvc.perform(post("/api/v1/study/start").with(csrf())
+        mockMvc.perform(post("/api/v1/study/session/start").with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request))
                 )
@@ -76,7 +76,7 @@ class StudySessionControllerTest {
                 .build();
 
         // when // then
-        mockMvc.perform(patch("/api/v1/study/end").with(csrf())
+        mockMvc.perform(patch("/api/v1/study/session/end").with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andDo(print())
@@ -93,7 +93,7 @@ class StudySessionControllerTest {
                 .build();
 
         // when // then
-        mockMvc.perform(post("/api/v1/study/start").with(csrf())
+        mockMvc.perform(post("/api/v1/study/session/start").with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andDo(print())
@@ -113,7 +113,7 @@ class StudySessionControllerTest {
                 .build();
 
         // when // then
-        mockMvc.perform(patch("/api/v1/study/end").with(csrf())
+        mockMvc.perform(patch("/api/v1/study/session/end").with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andDo(print())
@@ -133,7 +133,7 @@ class StudySessionControllerTest {
                 .build();
 
         // when // then
-        mockMvc.perform(patch("/api/v1/study/end").with(csrf())
+        mockMvc.perform(patch("/api/v1/study/session/end").with(csrf())
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andDo(print())
@@ -162,89 +162,12 @@ class StudySessionControllerTest {
                 .willReturn(response);
 
         // when // then
-        mockMvc.perform(get("/api/v1/study").with(csrf())
+        mockMvc.perform(get("/api/v1/study/session").with(csrf())
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.length()").value(2))
                 .andExpect(jsonPath("$.data[0].userId").value(1L))
                 .andExpect(jsonPath("$.data[0].nickname").value("김철수"));
-    }
-
-    @DisplayName("특정 날짜에 대한 공부 시간 조회")
-    @Test
-    void getStudyDuration() throws Exception {
-        // given
-        StudyDurationResponse result = StudyDurationResponse.builder().build();
-        LocalDate now = LocalDate.of(2024, 10, 1);
-
-        BDDMockito.given(studySessionService.getStudyDuration(now, 1L))
-                .willReturn(result);
-
-        // when // then
-        mockMvc.perform(
-                        get("/api/v1/study/duration").with(csrf())
-                                .contentType(MediaType.APPLICATION_JSON)
-                )
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @DisplayName("특정 연도와 월에 대한 공부 횟수 조회")
-    @Test
-    void getStudyCount() throws Exception {
-        // given
-        List<StudyCountLocalDateResponse> result = List.of();
-
-        BDDMockito.given(studySessionService.getStudyCount(2024, 9, 1L))
-                .willReturn(result);
-
-        // when // then
-        mockMvc.perform(
-                        get("/api/v1/study/count").with(csrf())
-                                .param("year", "2024")
-                                .param("month", "9")
-                                .contentType(MediaType.APPLICATION_JSON)
-                )
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @DisplayName("현재 날짜 기준으로 주단위 월, 일, 공부 횟수를 가져옵니다.")
-    @Test
-    void getStudyCountOfWeek() throws Exception {
-        // given
-        List<StudyCountResponse> result = List.of();
-
-        BDDMockito.given(studySessionService.getStudyCountOfWeek(LocalDate.now(), 1L))
-                .willReturn(result);
-
-        // when // then
-        mockMvc.perform(
-                        get("/api/v1/study/week").with(csrf())
-                                .contentType(MediaType.APPLICATION_JSON)
-                )
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @DisplayName("yearWeek를 파라미터로 해당 주의 랭킹을 구합니다.")
-    @Test
-    void getStudyDurationRanking() throws Exception {
-        // given
-        final int yearWeek = 202404;
-        WeeklyRankingResponse result = WeeklyRankingResponse.builder().build();
-
-        BDDMockito.given(studySessionService.getStudyDurationRanking(yearWeek))
-                .willReturn(result);
-
-        // when // then
-        mockMvc.perform(
-                        get("/api/v1/study/ranking").with(csrf())
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .param("yearWeek", String.valueOf(yearWeek))
-                )
-                .andDo(print())
-                .andExpect(status().isOk());
     }
 }
--- src/test/java/com/hongik/service/study/StudySessionServiceTest.java
+++ src/test/java/com/hongik/service/study/StudySessionServiceTest.java
@@ -6,7 +6,6 @@ import com.hongik.domain.user.Role;
 import com.hongik.domain.user.User;
 import com.hongik.domain.user.UserRepository;
 import com.hongik.dto.study.request.EndStudySessionRequest;
-import com.hongik.dto.study.request.StudySessionCreateRequest;
 import com.hongik.dto.study.request.StudySessionCreateRequest2;
 import com.hongik.dto.study.response.*;
 import org.junit.jupiter.api.AfterEach;
@@ -56,9 +55,9 @@ class StudySessionServiceTest {
         StudySessionStartResponse response = studySessionService.createStudy(request, user.getId());
 
         // then
-        assertThat(response.getId()).isNotNull();
+        assertThat(response.getStudySessionId()).isNotNull();
 
-        StudySession saved = studySessionRepository.findById(response.getId())
+        StudySession saved = studySessionRepository.findById(response.getStudySessionId())
                 .orElseThrow();
         assertThat(saved.getUser().getId()).isEqualTo(user.getId());
         assertThat(saved.getStartTime()).isEqualTo(now);
@@ -124,168 +123,6 @@ class StudySessionServiceTest {
         assertThat(response.isStudyStatus()).isFalse();
     }
 
-    @DisplayName("Param 값이 없을 때, 서버 시간 기준으로 유저의 날짜(Year, Month, Day)에 대한 공부 시간 조회")
-    @Test
-    void getStudyDuration() {
-        // given
-        LocalDate now = LocalDate.of(2024, 9, 20);
-        User user = createUser("username", "password", "nickname", "디자인학부");
-        userRepository.save(user);
-
-        /*
-        2024년 - 3시간 3분
-        2024년 9월 - 3시간 3분
-        2024년 9월 19일 - 2시간 2분, 2024년 9월 20일 - 1시간 1분
-         */
-        LocalDateTime startTime1 = LocalDateTime.of(2024, 9, 19, 10, 10);
-        LocalDateTime endTime1 = LocalDateTime.of(2024, 9, 19, 11, 11);
-        LocalDateTime startTime2 = LocalDateTime.of(2024, 9, 19, 20, 10);
-        LocalDateTime endTime2 = LocalDateTime.of(2024, 9, 19, 21, 11);
-        LocalDateTime startTime3 = LocalDateTime.of(2024, 9, 20, 20, 10);
-        LocalDateTime endTime3 = LocalDateTime.of(2024, 9, 20, 21, 11);
-        StudySession studySession1 = createStudySession(user, startTime1, endTime1);
-        StudySession studySession2 = createStudySession(user, startTime2, endTime2);
-        StudySession studySession3 = createStudySession(user, startTime3, endTime3);
-        studySessionRepository.saveAll(List.of(studySession1, studySession2, studySession3));
-
-        // when
-        StudyDurationResponse studyDurationResponse = studySessionService.getStudyDuration(now, user.getId());
-
-        // then
-        assertThat(studyDurationResponse).isNotNull();
-        assertThat(studyDurationResponse)
-                .extracting("yearHours", "yearMinutes")
-                .containsExactly(
-                        3L, 3L
-                );
-        assertThat(studyDurationResponse)
-                .extracting("monthHours", "monthMinutes")
-                .containsExactly(
-                        3L, 3L
-                );
-        assertThat(studyDurationResponse)
-                .extracting("dayHours", "dayMinutes")
-                .containsExactly(
-                        1L, 1L
-                );
-    }
-
-    @DisplayName("Param 값이 있을 때, 날짜(Year, Month, Day)에 대한 공부 시간 조회")
-    @Test
-    void getStudyDurationWithLocalDate() {
-        // given
-        LocalDate date = LocalDate.of(2024, 8, 20);
-        User user = createUser("username", "password", "nickname", "디자인학부");
-        userRepository.save(user);
-
-        /*
-        2024년 - 3시간 3분
-        2024년 8월 - 2시간 2분
-        2024년 8월 19일 - 1시간 1분
-        2024년 8월 20일 - 1시간 1분
-         */
-        LocalDateTime startTime1 = LocalDateTime.of(2024, 9, 19, 10, 10);
-        LocalDateTime endTime1 = LocalDateTime.of(2024, 9, 19, 11, 11);
-        LocalDateTime startTime2 = LocalDateTime.of(2024, 8, 19, 20, 10);
-        LocalDateTime endTime2 = LocalDateTime.of(2024, 8, 19, 21, 11);
-        LocalDateTime startTime3 = LocalDateTime.of(2024, 8, 20, 20, 10);
-        LocalDateTime endTime3 = LocalDateTime.of(2024, 8, 20, 21, 11);
-        StudySession studySession1 = createStudySession(user, startTime1, endTime1);
-        StudySession studySession2 = createStudySession(user, startTime2, endTime2);
-        StudySession studySession3 = createStudySession(user, startTime3, endTime3);
-        studySessionRepository.saveAll(List.of(studySession1, studySession2, studySession3));
-
-        // when
-        StudyDurationResponse studyDurationResponse = studySessionService.getStudyDuration(date, user.getId());
-
-        // then
-        assertThat(studyDurationResponse).isNotNull();
-        assertThat(studyDurationResponse)
-                .extracting("yearHours", "yearMinutes")
-                .containsExactly(
-                        3L, 3L
-                );
-        assertThat(studyDurationResponse)
-                .extracting("monthHours", "monthMinutes")
-                .containsExactly(
-                        2L, 2L
-                );
-        assertThat(studyDurationResponse)
-                .extracting("dayHours", "dayMinutes")
-                .containsExactly(
-                        1L, 1L
-                );
-    }
-
-    @DisplayName("유저의 특정 날짜(Month, Day)에 대한 공부 횟수 조회")
-    @Test
-    void getStudyCount() {
-        // given
-        User user = createUser("username", "password", "nickname", "디자인학부");
-        userRepository.save(user);
-
-        LocalDateTime startTime1 = LocalDateTime.of(2024, 9, 19, 10, 10);
-        LocalDateTime endTime1 = LocalDateTime.of(2024, 9, 19, 11, 10);
-        LocalDateTime startTime2 = LocalDateTime.of(2024, 9, 19, 20, 10);
-        LocalDateTime endTime2 = LocalDateTime.of(2024, 9, 19, 21, 10);
-        LocalDateTime startTime3 = LocalDateTime.of(2024, 9, 20, 20, 10);
-        LocalDateTime endTime3 = LocalDateTime.of(2024, 9, 20, 21, 10);
-        StudySession studySession1 = createStudySession(user, startTime1, endTime1);
-        StudySession studySession2 = createStudySession(user, startTime2, endTime2);
-        StudySession studySession3 = createStudySession(user, startTime3, endTime3);
-        studySessionRepository.saveAll(List.of(studySession1, studySession2, studySession3));
-
-        // when
-        List<StudyCountLocalDateResponse> result = studySessionService.getStudyCount(2024, 9, user.getId());
-
-        // then
-        assertThat(result).hasSize(2)
-                .extracting("date", "studyCount")
-                .containsExactlyInAnyOrder(
-                        tuple(LocalDate.of(2024, 9, 19), 2L),
-                        tuple(LocalDate.of(2024, 9, 20), 1L)
-                );
-    }
-
-    @DisplayName("현재 날짜 기준으로 주단위 월, 일, 공부 횟수를 가져옵니다. 순서 보장")
-    @Test
-    void getStudyCountOfWeek() {
-        // given
-        User user = createUser("username", "password", "nickname", "디자인학부");
-        userRepository.save(user);
-
-        // 9월 16일 월요일 기준 16 ~ 22일 데이터가 출력된다.
-        LocalDate today = LocalDate.of(2024, 9, 16);
-
-        LocalDateTime startTime1 = LocalDateTime.of(2024, 9, 19, 10, 10);
-        LocalDateTime endTime1 = LocalDateTime.of(2024, 9, 19, 11, 10);
-        LocalDateTime startTime2 = LocalDateTime.of(2024, 9, 19, 20, 10);
-        LocalDateTime endTime2 = LocalDateTime.of(2024, 9, 19, 21, 10);
-        LocalDateTime startTime3 = LocalDateTime.of(2024, 9, 20, 20, 10);
-        LocalDateTime endTime3 = LocalDateTime.of(2024, 9, 20, 21, 10);
-        StudySession studySession1 = createStudySession(user, startTime1, endTime1);
-        StudySession studySession2 = createStudySession(user, startTime2, endTime2);
-        StudySession studySession3 = createStudySession(user, startTime3, endTime3);
-        studySessionRepository.saveAll(List.of(studySession1, studySession2, studySession3));
-
-        // when
-        List<StudyCountResponse> result = studySessionService.getStudyCountOfWeek(today, user.getId());
-
-        // then
-        assertThat(result).hasSize(7)
-                .extracting("date", "studyCount")
-                .containsExactly(
-                        tuple("9/16", 0L),
-                        tuple("9/17", 0L),
-                        tuple("9/18", 0L),
-                        tuple("9/19", 2L),
-                        tuple("9/20", 1L),
-                        tuple("9/21", 0L),
-                        tuple("9/22", 0L)
-
-                );
-    }
-
 //    @DisplayName("원하는 날짜에 대한 랭킹을 조회한다. ex) 9월 3주차 YearWeek = 202438")
 //    @Test
 //    void getStudyDurationRanking() {
@@ -332,14 +169,6 @@ class StudySessionServiceTest {
                 .role(Role.USER)
                 .department(department)
                 .nickname(nickname)
-                .build();
-    }
-
-    private StudySession createStudySession(User user, final LocalDateTime startTime, final LocalDateTime endTime) {
-        return StudySession.builder()
-                .user(user)
-                .startTime(startTime)
-                .endTime(endTime)
                 .build();
     }
 
