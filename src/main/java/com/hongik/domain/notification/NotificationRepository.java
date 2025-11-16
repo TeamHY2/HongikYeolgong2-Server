4dcf4f856e92736072935ae4fa120f77acfc468e
sunkon25 <kcindy325k@kyonggi.ac.kr>
2025-11-17 00:15:33 +0900

    fix: 알림 엔티티 수정

--- src/main/java/com/hongik/domain/notification/NotificationRepository.java
+++ src/main/java/com/hongik/domain/notification/NotificationRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<Long, Notification> {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }
--- src/main/java/com/hongik/domain/notification/NotificationType.java
+++ src/main/java/com/hongik/domain/notification/NotificationType.java
@@ -1,5 +1,5 @@
 package com.hongik.domain.notification;
 
 public enum NotificationType {
-
+	REQUEST, ACCEPTED
 }
