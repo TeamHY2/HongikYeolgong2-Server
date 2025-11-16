ca915c9945f2176a00492cb1b12582aa9d9662b8
sunkon25 <kcindy325k@kyonggi.ac.kr>
2025-11-17 00:15:53 +0900

    fix: 차단 레포지토리 수정

--- src/main/java/com/hongik/domain/block/BlockRepository.java
+++ src/main/java/com/hongik/domain/block/BlockRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlockRepository extends JpaRepository<Long, Block> {
+public interface BlockRepository extends JpaRepository<Block, Long> {
 }
